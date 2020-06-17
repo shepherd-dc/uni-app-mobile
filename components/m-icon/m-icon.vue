bd41fb2f5b25ab91eae77b4a7372b71c849dc50a
shepherd_dc <shepherd_dc@aliyun.com>
2020-06-17 11:42:35 +0800

    uni-app init

---
+++ components/m-icon/m-icon.vue
@@ -0,0 +1,26 @@
+<template>
+  <view
+    :class="['m-icon-'+type]"
+    class="m-icon"
+    @click="onClick()"></view>
+</template>
+
+<script>
+export default {
+  props: {
+    /**
+			 * 图标类型
+			 */
+    type: String
+  },
+  methods: {
+    onClick () {
+      this.$emit('click')
+    }
+  }
+}
+</script>
+
+<style>
+	@import "./m-icon.css";
+</style>
