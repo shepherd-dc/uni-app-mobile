72ca97306122ae5f1f750b932341e9003d6ec7f6
shepherd_dc <shepherd_dc@aliyun.com>
2021-05-25 14:56:27 +0800

    split growth page

--- pages/mine/mine.vue
+++ pages/mine/mine.vue
@@ -30,19 +30,19 @@
           type="userNickName"></open-data>
       </view>
 			
-			<view class="user-level-growth" v-if="inited">
-				<xc-level
-					:myInfo="myInfo"
-					:myLevel="myLevel"
-					:nextLevel="nextLevel"
-					:growthValueToNextLevel="growthValueToNextLevel" />
-				
-				<xc-growth
-					:levels="levels"
-					:selectedId="selectedId"
-					:selectedLevel="selectedLevel"
-					@on-item-click="onLevelItemClick" />
-				
+			<view class="growth-point">
+				<view class="flex point" @click="navigateToPage('point')">
+					<text class="num">10</text>
+					<text class="txt">积分</text>
+				</view>
+				<view class="flex like">
+					<text class="num">100</text>
+					<text class="txt">获赞</text>
+				</view>
+				<view class="flex growth" @click="navigateToPage('growth')">
+					<text class="num">1500</text>
+					<text class="txt">成长值</text>
+				</view>
 			</view>
 			
       <view class="btn-row">
@@ -54,42 +54,19 @@
 		<view class="btn-row">
 			<button type="default" @click="test">账号登录测试</button>
 		</view>
-		<view class="btn-row">
-			<button type="default" @click="syncGrowthValue">同步微信运动数据 {{ stepInfoList.step }}</button>
-		</view>
   </view>
 </template>
 
 <script>
 import { mapState, mapActions } from 'vuex'
 import { loginCheck } from '@/utils/loginCheck'
-import LevelService from '@/service/LevelService'
-import UserService from '@/service/UserService'
 
 export default {
   onLoad () {
     loginCheck()
   },
-	onShow () {
-		if (this.hasLogin) this.getLevelList()
-	},
-	onReady () {
-		// //用户授权读取微信运动数据
-		// this.authorizeWeRun()
-	},
 	data() {
 		return {
-			levels: [],
-			myInfo: {},
-			myLevel: {},
-			selectedId: undefined,
-			selectedLevel: undefined,
-			growthValueToNextLevel: 0,
-			nextLevel: undefined,
-			stepInfoList: {
-				step: 0
-			},
-			inited: false
 		}
 	},
   computed: {
@@ -116,135 +93,10 @@ export default {
 			  url: '../user/login'
 			})
 		},
-		syncGrowthValue () {
-			//用户授权读取微信运动数据
-			this.authorizeWeRun()
-		},
-		/**
-		 * 用户授权读取微信运动数据
-		 */
-		authorizeWeRun () {
-			const that = this
-			// 首先获取用户的授权状态
-			// uni.getSetting的说明详见
-			// https://uniapp.dcloud.net.cn/api/other/authorize
-			// https://developers.weixin.qq.com/miniprogram/dev/api/wx.getSetting.html
-			uni.getSetting({
-				success(res) {
-					if (!res.authSetting['scope.werun']) {
-						// 如果用户还未授权过，需要用户授权读取微信运动数据
-						// uni.authorize详见
-						// https://uniapp.dcloud.net.cn/api/other/authorize
-						// https://developers.weixin.qq.com/miniprogram/dev/api/open-api/authorize/wx.authorize.html
-						uni.authorize({
-							scope: 'scope.werun',
-							success() {
-								that.getWeRunData()
-							},
-							fail() {
-								//如果用户拒绝授权，提示用户需要同意授权才能获取他的微信运动数据
-								uni.showModal({
-									title: '读取微信运动数据失败',
-									content: '请在小程序设置界面（「右上角」 - 「关于」 - 「右上角」 - 「设置」）中允许我们访问微信运动数据',
-								})
-							}
-						})
-					} else {
-						//如果用户已授权过，直接开始同步微信运动数据
-						that.getWeRunData()
-					}
-				}
+		navigateToPage (p) {
+			uni.navigateTo({
+				url: `/pages/mine/${p}`
 			})
-		},
-		/**
-		 * 获取微信运动数据
-		 */
-		getWeRunData () {
-			const that = this
-			// 调用微信运动API获取用户最近30天的运动步数，详见https://developers.weixin.qq.com/miniprogram/dev/api/wx.getWeRunData.html
-			wx.getWeRunData({
-				success(weRunEncryptedData) {
-					// 调用云函数，获得解密后的微信运动步数
-					that.getStepInfoList(weRunEncryptedData)
-				}
-			})
-		},
-		/**
-		 * 调用云函数，获取解密后的微信运动步数
-		 */
-		async getStepInfoList (weRunEncryptedData) {
-			const { encryptedData, iv } = weRunEncryptedData
-			const jscode = await this.getTempCode()
-			//云调用获取解密后的开放数据
-			uniCloud.callFunction({
-				name: 'weRunData',
-				data: {
-					js_code: jscode,
-					encryptedData,
-					iv
-				},
-				success: (res) => {
-					console.log('weRunData', res.result[res.result.length - 1])
-					//保存调用云函数得到的解密后微信运动步数，用于显示
-					this.stepInfoList = res.result[res.result.length - 1]
-				},
-			})
-		},
-		getTempCode (value = 'weixin') {
-			return new Promise((reslove, reject) => {
-				uni.login({
-					provider: value,
-					success: (res) => {
-						reslove(res.code)
-					},
-					fail: (err) => {
-						reject(err)
-					}
-				})
-			})
-		},
-		async getOpenid () {
-			const jscode = await this.getTempCode()
-			const { data } = await this.$uniCloud('loginByWechat', {
-			  js_code: jscode
-			})
-			return data.openid
-		},
-		async getLevelList () {
-			const levelService = new LevelService()
-			const levelList = await levelService.getLevelList()
-			console.log('getLevelList', levelList)
-			const levels = levelList.data
-			
-			const openid = await this.getOpenid()
-			const userService = new UserService()
-			const userInfo = await userService.getUserInfo(openid)
-			console.log('getUserInfo', userInfo)
-			const myInfo = userInfo.data[0]
-			this.setLevelData(levels, myInfo)
-		},
-		setLevelData (levels, myInfo) {
-			const myLevel = levels.filter(e => e.minGrowthValue <= myInfo.growthValue && myInfo.growthValue <= e.maxGrowthValue )[0]
-			this.levels = levels
-			this.myLevel = myLevel
-			this.myInfo = myInfo
-			this.selectedId = myLevel.id
-			this.selectedLevel = levels[myLevel.id - 1]
-			
-			const nextLevel = levels.filter(e => e.id == myLevel.id + 1)[0]
-			if (nextLevel !== undefined) {
-				this.growthValueToNextLevel = nextLevel.minGrowthValue - myInfo.growthValue,
-				this.nextLevel = nextLevel
-			}
-			//显示界面
-			this.inited = true
-		},
-		onLevelItemClick (id) {
-			//如果切换成长等级才响应
-			if (this.selectedId != id) {
-				this.selectedId = id
-				this.selectedLevel = this.levels[id - 1]
-			}
 		}
   }
 }
@@ -269,5 +121,24 @@ export default {
       font-size: 32rpx;
     }
   }
+}
+.growth-point {
+	display: flex;
+	justify-content: space-between;
+	background-color: #fff;
+	margin: 20rpx 0;
+	padding: 30rpx 40rpx;
+	.flex {
+		display: flex;
+		flex-direction: column;
+		line-height: 50rpx;
+		.num {
+			font-size: 36rpx;
+			font-weight: bold;
+		}
+		.txt {
+			color: #999;
+		}
+	}
 }
 </style>
---
+++ pages/mine/point.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		积分
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
